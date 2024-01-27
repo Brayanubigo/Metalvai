88fba4874fa1dbe7c1c39633769f181569f6cf9c
Brayanubigo <brayan.ubigo@gmail.com>
2024-01-27 10:03:58 -0300

    change /

--- metalvai_Backend/index.js
+++ metalvai_Backend/index.js
@@ -27,9 +27,7 @@ dotenv.config();
 
 //app.use(cors(corsOption))
 
-// app.get("/",(req,res)=>
-// {const htmlResponse="<html><head><body>olaaaaaa</body></head></html>";
-//  res.send(htmlResponse)})
+
 
 
 app.use(admin)
